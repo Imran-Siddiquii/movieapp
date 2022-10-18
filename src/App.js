3e869b450e7d93c9e123a2598cd69e728b2c9430
Imran-Siddiquii <siddiquiimran132@gmail.com>
2022-10-18 23:10:01 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,63 @@
-import logo from './logo.svg';
 import './App.css';
-
+import React, { useEffect, useState } from 'react';
+import Home from './Home'
+import {Row,Col,Card, Button, Modal} from  'antd';
+const { Meta } = Card;
 function App() {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [movies,setMovie]=useState([])
+
+
+
+  const showModal = () => {
+    setIsModalOpen(true);
+  };
+
+  const handleOk = () => {
+    setIsModalOpen(false);
+  };
+
+  const handleCancel = () => {
+    setIsModalOpen(false);
+  };
+useEffect(()=>{
+  fetch('https://movie-task.vercel.app/api/popular?page=1').then(res=>res.json()).then(json=>{
+  console.log(json.data.results)  
+  setMovie(json.data.results)})
+},[])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <Row gutter={{ xs: 8, sm: 16, md: 24, lg: 32 }} style={{display:"flex"}}>
+  {movies.map((list,key)=>(
+    <div key={list.id}>
+      <Col className=""  span={6}>
+      <Card
+    hoverable
+    style={{
+      width: 240,
+    }}>
+  {/* <img src=`${list.poster_path}` alt="example" /> */}
+    <Meta title="Europe Street beat" description="www.instagram.com" />
+  </Card>
+      </Col>
+      </div>
+  ))}
+</Row>     
+
+
+
+
+
+      <Button type="primary" onClick={showModal}>
+        Open Modal
+      </Button>
+      <Modal title="Basic Modal" open={isModalOpen} onOk={handleOk} onCancel={handleCancel}>
+        <p>Some contents...</p>
+        <p>Some contents...</p>
+        <p>Some contents...</p>
+      </Modal> <Home/>
+    
+    </>
   );
 }
 
---
+++ src/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div>
+        <div className="btn btn-info">hello</div>
+    </div>
+  )
+}
+
+export default Home
