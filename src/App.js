3090c01dd11ad7379406863cdfb2648e8c6ae024
Imran-Siddiquii <siddiquiimran132@gmail.com>
2022-10-20 11:39:28 +0530

    filter function

--- src/App.js
+++ src/App.js
@@ -6,12 +6,8 @@ const { Meta } = Card;
 function App() {
   const [isModalOpen, setIsModalOpen] = useState(false);
   const [movies,setMovie]=useState([])
-
-
-
-  const showModal = () => {
-    setIsModalOpen(true);
-  };
+  const [search,setSearch]=useState( )
+  const [idMovie,setIdMovie]=useState([])
 
   const handleOk = () => {
     setIsModalOpen(false);
@@ -25,37 +21,58 @@ useEffect(()=>{
   console.log(json.data.results)  
   setMovie(json.data.results)})
 },[])
+const onChangehandler=e=>{
+  const filter=movies.filter((val)=>val.release_date.slice(0,4)==e.target.value)
+  if(filter.length==0){
+    console.log("hello")
+  }
+  setSearch(filter)
+  console.log("filter by year",filter)
+}
   return (
     <>
-    <Row gutter={{ xs: 8, sm: 16, md: 24, lg: 32 }} style={{display:"flex"}}>
-  {movies.map((list,key)=>(
+    <h3 className='text-center'>Movies List</h3>
+    <h5 className='text-center py-1'><input className='' onChange={onChangehandler}/></h5>
+  <div className='container border border-1'>
+    <Row className='justify-content-center' gutter={{ xs: 8, sm: 16, md: 24, lg: 32 }} >
+  {(search || movies)?(search||movies).map((list,key)=>(
     <div key={list.id}>
-      <Col className=""  span={6}>
+      <Col className="" span={6}>
       <Card
     hoverable
     style={{
       width: 240,
     }}>
-  {/* <img src=`${list.poster_path}` alt="example" /> */}
-    <Meta title="Europe Street beat" description="www.instagram.com" />
+  <img src={`${"https://image.tmdb.org/t/p/original/"+list.poster_path}`} alt="example" width="100%" height="175px" />
+  <p className='my-1'>Movie Name : {list.title}</p>
+  <p>Movie language : {list.original_language}</p>
+  <p>Release : {list.release_date
+}</p>
+  <Button type="primary" onClick={()=>{
+    setIsModalOpen(true)
+    setIdMovie(list)
+  }
+  }>
+        More Info
+      </Button>
   </Card>
       </Col>
       </div>
-  ))}
+  )):(search.length===0)?<div>
+      <Col className="" span={6}>
+      <Card
+    hoverable
+    style={{
+      width: 240,
+    }}>
+  
+        <p>hello</p>
+  </Card>
+      </Col>
+      </div>:null}
 </Row>     
-
-
-
-
-
-      <Button type="primary" onClick={showModal}>
-        Open Modal
-      </Button>
-      <Modal title="Basic Modal" open={isModalOpen} onOk={handleOk} onCancel={handleCancel}>
-        <p>Some contents...</p>
-        <p>Some contents...</p>
-        <p>Some contents...</p>
-      </Modal> <Home/>
+</div>
+       <Home data={idMovie} open={isModalOpen} onOk={handleOk} onCancel={handleCancel}/>
     
     </>
   );
--- src/Home.jsx
+++ src/Home.jsx
@@ -1,9 +1,21 @@
 import React from 'react'
-
-const Home = () => {
+import {Modal} from 'antd'
+const Home = ({props,data,open,onOk,onCancel}) => {
   return (
     <div>
-        <div className="btn btn-info">hello</div>
+        <Modal title={data.title} open={open} onOk={onOk} onCancel={onCancel}>
+      <img src={`${"https://image.tmdb.org/t/p/original/"+data.poster_path}`} alt="example" width="100%" height="175px" />
+  <p className='my-1'>Movie Name : {data.title}</p>
+  <p>Movie language : {data.original_language}</p>
+  <p>Description : {data.overview
+}</p>
+<p>Release Date : {data.release_date}</p>
+<p>Vote : {data.vote_count}</p>
+
+
+
+  <p>Popularity : {data.popularity}</p>
+      </Modal>
     </div>
   )
 }
